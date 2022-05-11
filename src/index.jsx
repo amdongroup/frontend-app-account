2784a77b8e141e04817fc778293c00ddc564a77b
thiha <thiha.naing.ucsm@gmail.com>
2022-05-11 14:48:55 +0630

    Custom Header Testing

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Switch } from 'react-router-dom';
 
-import Header, { messages as headerMessages } from 'frontend-component-header-oxygen';
+import Header, { messages as headerMessages } from '@edx/frontend-component-header';
 import Footer, { messages as footerMessages } from '@edx/frontend-component-footer';
 
 import configureStore from './data/configureStore';
